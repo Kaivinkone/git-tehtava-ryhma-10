fe25984b279e291c60cf4f0906d844d64c1a4e1a
Hildura <hilda-mari.myllymaki@student.hamk.fi>
2023-11-30 16:35:53 +0200

    Attribuutin luonti luokan sisälle

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-public class Main{
-    public static void main(String[] args){
-        //luodaan uusi olio
+public class Main {
+    public static void main(String[] args) {
+        // luodaan uusi olio
         Kahvinkeitin mocca = new Kahvinkeitin();
         mocca.vari = "Pinkki";
         mocca.merkki = "MoccaMasteri";
@@ -8,26 +8,27 @@ public class Main{
     }
 }// Main class end here
 
-class Kahvinkeitin{
+class Kahvinkeitin {
     String vari;
     String merkki;
-    
-    //Construction
-    public Kahvinkeitin(){
-    	vari = "musta";
-    	merkki = "Electrolux";
+    int maara;
+
+    // Construction
+    public Kahvinkeitin() {
+        vari = "musta";
+        merkki = "Electrolux";
     }
 
-    public void PaallePois(boolean on){
-        if (on == true){
+    public void PaallePois(boolean on) {
+        if (on == true) {
             System.out.println("Kahvinkeitin on päällä.");
         }
-        if (on == false){
+        if (on == false) {
             System.out.println("Kahvnikeitin on sammuksi.");
         }
     }
 
-    public void TulostaTiedot(){
+    public void TulostaTiedot() {
         System.out.println("*** Kahvinkeittimen tiedot ***");
         System.out.println("Keittimen väri: " + vari);
         System.out.println("Keittimen merkki: " + merkki);
